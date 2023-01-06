713dfc03cf181b5ce330bb7e54beff608a757d4a
elvinelk <100845332+elvinelk@users.noreply.github.com>
2023-01-06 12:35:47 +0100

    update lake import / aws credentials
    update lake import / aws credentials

--- lambda_functions/load_measuring_points_coordinates_into_RDS_lake_LR.py
+++ lambda_functions/load_measuring_points_coordinates_into_RDS_lake_LR.py
@@ -58,14 +58,9 @@ def lambda_handler(event, context):
     s3_client = boto3.client('s3')
     s3_bucket_name = 'electricbucket'
     s3 = boto3.resource('s3',
-                        aws_access_key_id='ASIAWGCGFD37GHUUWS5R',
-                        aws_secret_access_key='CKg4nXkXpauLmCy9K8EC9FF6p6tTo43rHWuXQm8U',
-                        aws_session_token='FwoGZXIvYXdzELX//////////wEaDM7OLIvyk10aL+7Y0y\
-                        K2AW65B5LT6v85nFxH/Ia3LedLBNOpXd3eiyPah42XPC6dh7PfPJ+r/KL4bd6m368\
-                        ukxmURIT9ssJSBPyKipVyJK5Vo3K/lElp5ztgXNISdrP1IcTc8+TKIooa3N2lIGEl\
-                        dau38LyWtvWghyxH+TxoyBKgwNxAfPwz/mNcJVHpxjjfFTughXcLi/Ad6R4KUDt6R\
-                        mYDgHUZrhkYmpP7LejqU40UuXDuJbhLs/6A4VCsTz9jBpVXPC1sKObH9JsGMi06CT\
-                        kz1H3kDoBxrbecPvU80WfpHQ1pihCCTaEqvWixVjZAEE9MpvLhMq0EyKQ=')
+                        aws_access_key_id= creds.aws_access_key_id,
+                        aws_secret_access_key= creds.aws_secret_access_key,
+                        aws_session_token= creds.aws_session_token)
 
     my_bucket = s3.Bucket(s3_bucket_name)
 
